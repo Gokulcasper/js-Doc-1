61ae8c1607fa9b0b5391de982460c5e58feb3caa
Gokulcasper <ronitcasper215@gmail.com>
2024-07-09 14:55:37 +0530

    commit Element ID

---
+++ index.js
@@ -0,0 +1,76 @@
+function changeHeading(){
+    document.getElementById("web-technology").textContent="Front-End Technology";
+    document.getElementById("web-technology").style.color="red";
+}
+
+
+function dhoni(){
+    document.getElementById("fvrt-img").src="https://documents.iplt20.com/ipl/IPLHeadshot2024/57.png ";
+    document.getElementById("dhoni").style.backgroundColor="green";
+    document.getElementById("dhoni").style.color="white";
+    document.getElementById("dhoni").style.borderColor="green"
+
+    document.getElementById("tendulkar").style.backgroundColor="red";
+    document.getElementById("tendulkar").style.borderColor="red";
+
+    document.getElementById("yuvaraj").style.backgroundColor="red";
+    document.getElementById("yuvaraj").style.borderColor="red";
+
+    document.getElementById("kohli").style.backgroundColor="red";
+    document.getElementById("kohli").style.borderColor="red";
+
+    
+
+
+}
+
+function tendulkar(){
+    document.getElementById("fvrt-img").src="https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_320,q_50/lsci/db/PICTURES/CMS/316400/316486.png";
+    document.getElementById("tendulkar").style.backgroundColor="green";
+    document.getElementById("tendulkar").style.color="white";
+    document.getElementById("tendulkar").style.borderColor="green"
+
+    document.getElementById("dhoni").style.backgroundColor="red";
+    document.getElementById("dhoni").style.borderColor="red";
+
+    document.getElementById("yuvaraj").style.backgroundColor="red";
+    document.getElementById("yuvaraj").style.borderColor="red";
+
+    document.getElementById("kohli").style.backgroundColor="red";
+    document.getElementById("kohli").style.borderColor="red";
+
+}
+
+function yuvaraj(){
+    document.getElementById("fvrt-img").src="https://documents.iplt20.com/playerheadshot/ipl/284/113.png";
+    document.getElementById("yuvaraj").style.backgroundColor="green";
+    document.getElementById("yuvaraj").style.color="white";
+    document.getElementById("yuvaraj").style.borderColor="green"
+
+    document.getElementById("dhoni").style.backgroundColor="red";
+    document.getElementById("dhoni").style.borderColor="red";
+
+    document.getElementById("tendulkar").style.backgroundColor="red";
+    document.getElementById("tendulkar").style.borderColor="red";
+
+    document.getElementById("kohli").style.backgroundColor="red";
+    document.getElementById("kohli").style.borderColor="red";
+
+}
+
+function kohli(){
+    document.getElementById("fvrt-img").src="https://documents.iplt20.com/ipl/IPLHeadshot2024/2.png";
+    document.getElementById("kohli").style.backgroundColor="green";
+    document.getElementById("kohli").style.color="white";
+    document.getElementById("kohli").style.borderColor="green"
+
+    document.getElementById("dhoni").style.backgroundColor="red";
+    document.getElementById("dhoni").style.borderColor="red";
+
+    document.getElementById("yuvaraj").style.backgroundColor="red";
+    document.getElementById("yuvaraj").style.borderColor="red";
+
+    document.getElementById("tendulkar").style.backgroundColor="red";
+    document.getElementById("tendulkar").style.borderColor="red";
+
+}
